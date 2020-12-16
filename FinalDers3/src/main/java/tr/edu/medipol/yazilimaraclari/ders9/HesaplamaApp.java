a53d9026462cd4666955f4c3a8bc535c79a6949b
deryacayiroglu <deryacayroglu@gmail.com>
2020-12-16 21:17:15 +0300

    bolme eklendi

--- FinalDers3/src/main/java/tr/edu/medipol/yazilimaraclari/ders9/HesaplamaApp.java
+++ FinalDers3/src/main/java/tr/edu/medipol/yazilimaraclari/ders9/HesaplamaApp.java
@@ -18,5 +18,8 @@ public class HesaplamaApp {
 	public static int cikarma(int sayi1,int sayi2) {
 		return sayi1-sayi2;
 	}
+	public static int bolme(int sayi1,int sayi2) {
+		return sayi1/sayi2;
+	}
 
 }
